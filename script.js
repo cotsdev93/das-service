e746156deb9505f89f74a7dd510b0f80be356159
cotsdev93 <cotsdev93@gmail.com>
2024-09-01 18:40:00 -0300

    animaciones de sections

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
 document.querySelectorAll("nav li").forEach((item) => {
-    item.addEventListener("click", (e) => {
+  item.addEventListener("click", (e) => {
+    e.preventDefault();
 
-      e.preventDefault(); // Evita que el enlace recargue la página
-  
-      // Obtener el ID de la sección que se debe mostrar
-      const sectionId = e.target.closest("li").getAttribute("data-section");
-  
-      // Remover la clase 'active' de la sección actualmente visible
-      document.querySelector(".section.active").classList.remove("active");
-  
-      // Agregar la clase 'active' a la nueva sección
-      document.getElementById(`section${sectionId}`).classList.add("active");
-  
-      // Mover el contenedor al porcentaje adecuado para crear el efecto de carrusel
-    //   document.querySelector(".sectionsContainer").style.transform = `translateX(-${(sectionId - 1) * 100}%)`;
-      console.log("funca")
-    });
+    const sectionId = e.target.closest("li").getAttribute("data-section");
+
+    const currentActiveSection = document.querySelector(".section.active");
+    if (currentActiveSection) {
+      currentActiveSection.classList.remove("active");
+    }
+
+    document.querySelector(
+      ".sectionsContainer"
+    ).style.transform = `translateX(-${(sectionId - 1) * 100}vw)`;
+
+    setTimeout(() => {
+      const newActiveSection = document.getElementById(`section${sectionId}`);
+      if (newActiveSection) {
+        newActiveSection.classList.add("active");
+      }
+    }, 100);
   });
-  
+});
