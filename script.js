195946d8c169144169502a19dd3be4c397f99be1
cotsdev93 <cotsdev93@gmail.com>
2024-09-07 18:26:54 -0300

    agregado libreria google review

--- script.js
+++ script.js
@@ -34,68 +34,66 @@ document.querySelectorAll("nav li").forEach((item) => {
 });
 
 // CLIENTES
-const placeId = "ChIJbfOMr3PIvJURJ2vXCwQqGzI";
-const apiKey = "AIzaSyB2qQJFxptyNJyBkyQD1bzZHg-sr3V07Tw";
+// const placeId = "ChIJbfOMr3PIvJURJ2vXCwQqGzI";
+// const apiKey = "AIzaSyB2qQJFxptyNJyBkyQD1bzZHg-sr3V07Tw";
 
-async function getGoogleReviews() {
-  try {
-    const response = await fetch("http://localhost:3000/google-reviews");
-    const data = await response.json();
+// async function getGoogleReviews() {
+//   try {
+//     const response = await fetch("http://localhost:3000/google-reviews");
+//     const data = await response.json();
 
-    console.log(data); // Verifica la estructura de la respuesta
+//     console.log(data); // Verifica la estructura de la respuesta
 
-    const reviews = data.result?.reviews; // Accede a las reseñas dentro de data.result
-    if (reviews && Array.isArray(reviews)) {
-      const reviewsElement = document.getElementById("reviews");
-      reviewsElement.innerHTML = ""; // Limpiar contenido anterior
+//     const reviews = data.result?.reviews; // Accede a las reseñas dentro de data.result
+//     if (reviews && Array.isArray(reviews)) {
+//       const reviewsElement = document.getElementById("reviews");
+//       reviewsElement.innerHTML = ""; // Limpiar contenido anterior
 
-      reviews.forEach((review) => {
-        const reviewElement = document.createElement("p");
-        reviewElement.textContent = review.text;
-        reviewsElement.appendChild(reviewElement);
-      });
-    } else {
-      console.error("No se encontraron reseñas.");
-    }
-  } catch (error) {
-    console.error("Error al obtener las calificaciones:", error);
-  }
-}
+//       reviews.forEach((review) => {
+//         const reviewElement = document.createElement("p");
+//         reviewElement.textContent = review.text;
+//         reviewsElement.appendChild(reviewElement);
+//       });
+//     } else {
+//       console.error("No se encontraron reseñas.");
+//     }
+//   } catch (error) {
+//     console.error("Error al obtener las calificaciones:", error);
+//   }
+// }
 
-// document.getElementById("reviews").innerHTML = "Some content";
+// function initMap() {
+//   async function getPlaceDetails(Place) {
+//     try {
+//       // Crea una nueva instancia de Place usando el Place ID.
+//       const place = new google.maps.places.Place({
+//         id: "ChIJbfOMr3PIvJURJ2vXCwQqGzI",
+//         requestedLanguage: "en",
+//       });
 
-// Función para inicializar y cargar la API de Google Maps y Places
-function initMap() {
-  async function getPlaceDetails(Place) {
-    try {
-      // Crea una nueva instancia de Place usando el Place ID.
-      const place = new google.maps.places.Place({
-        id: "ChIJbfOMr3PIvJURJ2vXCwQqGzI",
-        requestedLanguage: "en",
-      });
+//       // Llama a fetchFields, pasando los campos de datos deseados.
+//       await place.fetchFields({ fields: ["displayName", "formattedAddress"] });
 
-      // Llama a fetchFields, pasando los campos de datos deseados.
-      await place.fetchFields({ fields: ["displayName", "formattedAddress"] });
+//       // Verifica si los campos están definidos.
+//       if (place.displayName && place.formattedAddress) {
+//         console.log("Nombre del lugar:", place.displayName);
+//         console.log("Dirección formateada:", place.formattedAddress);
+//       } else {
+//         console.error(
+//           "No se encontraron detalles para el Place ID proporcionado."
+//         );
+//       }
+//     } catch (error) {
+//       console.error("Error al obtener los detalles del lugar:", error);
+//     }
+//   }
 
-      // Verifica si los campos están definidos.
-      if (place.displayName && place.formattedAddress) {
-        console.log("Nombre del lugar:", place.displayName);
-        console.log("Dirección formateada:", place.formattedAddress);
-      } else {
-        console.error(
-          "No se encontraron detalles para el Place ID proporcionado."
-        );
-      }
-    } catch (error) {
-      console.error("Error al obtener los detalles del lugar:", error);
-    }
-  }
+//   // Llama a la función y verifica el resultado
+//   getPlaceDetails(google.maps.places.Place);
+// }
 
-  // Llama a la función y verifica el resultado
-  getPlaceDetails(google.maps.places.Place);
-}
-// Espera a que el DOM esté completamente cargado
-document.addEventListener("DOMContentLoaded", function () {
-  initMap();
-  getGoogleReviews();
-});
+// // Espera a que el DOM esté completamente cargado
+// document.addEventListener("DOMContentLoaded", function () {
+//   initMap();
+//   getGoogleReviews();
+// });
