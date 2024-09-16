6039212e1a43263ac8774a29abad5ff7321fd1d7
cotsdev93 <cotsdev93@gmail.com>
2024-09-16 16:02:38 -0300

    correccion sections animation

--- script.js
+++ script.js
@@ -39,7 +39,7 @@ document.querySelectorAll("nav li").forEach((item) => {
 
       // Desactivamos el flag de animación después de que todas las animaciones hayan terminado.
       isAnimating = false;
-    }, 4000); // La duración debe coincidir con la duración de la animación.
+    }, 2000); // Ajusta la duración para que coincida con la duración de las animaciones.
   });
 });
 
@@ -353,70 +353,3 @@ btnCategoriaProductos.forEach((boton) => {
     cargarProductos(productos);
   });
 });
-
-// CLIENTES
-// const placeId = "ChIJbfOMr3PIvJURJ2vXCwQqGzI";
-// const apiKey = "AIzaSyB2qQJFxptyNJyBkyQD1bzZHg-sr3V07Tw";
-
-// async function getGoogleReviews() {
-//   try {
-//     const response = await fetch("http://localhost:3000/google-reviews");
-//     const data = await response.json();
-
-//     console.log(data); // Verifica la estructura de la respuesta
-
-//     const reviews = data.result?.reviews; // Accede a las reseñas dentro de data.result
-//     if (reviews && Array.isArray(reviews)) {
-//       const reviewsElement = document.getElementById("reviews");
-//       reviewsElement.innerHTML = ""; // Limpiar contenido anterior
-
-//       reviews.forEach((review) => {
-//         const reviewElement = document.createElement("p");
-//         reviewElement.textContent = review.text;
-//         reviewsElement.appendChild(reviewElement);
-//       });
-//     } else {
-//       console.error("No se encontraron reseñas.");
-//     }
-//   } catch (error) {
-//     console.error("Error al obtener las calificaciones:", error);
-//   }
-// }
-
-// function initMap() {
-//   async function getPlaceDetails(Place) {
-//     try {
-//       // Crea una nueva instancia de Place usando el Place ID.
-//       const place = new google.maps.places.Place({
-//         id: "ChIJbfOMr3PIvJURJ2vXCwQqGzI",
-//         requestedLanguage: "en",
-//       });
-
-//       // Llama a fetchFields, pasando los campos de datos deseados.
-//       await place.fetchFields({ fields: ["displayName", "formattedAddress"] });
-
-//       // Verifica si los campos están definidos.
-//       if (place.displayName && place.formattedAddress) {
-//         console.log("Nombre del lugar:", place.displayName);
-//         console.log("Dirección formateada:", place.formattedAddress);
-//       } else {
-//         console.error(
-//           "No se encontraron detalles para el Place ID proporcionado."
-//         );
-//       }
-//     } catch (error) {
-//       console.error("Error al obtener los detalles del lugar:", error);
-//     }
-//   }
-
-//   // Llama a la función y verifica el resultado
-//   getPlaceDetails(google.maps.places.Place);
-// }
-
-// // Espera a que el DOM esté completamente cargado
-// document.addEventListener("DOMContentLoaded", function () {
-//   initMap();
-//   getGoogleReviews();
-// });
-
-// EQUIPOS
